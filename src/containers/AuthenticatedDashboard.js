30e0014605f1c0f19c4fc8281e2285cd47b1ce56
Abdel <abdelshokair@gmail.com>
2020-04-29 04:23:10 -0400

    Loading effect set up in UI while data is fetched

--- src/containers/AuthenticatedDashboard.js
+++ src/containers/AuthenticatedDashboard.js
@@ -9,42 +9,65 @@ import PropTypes from 'prop-types';
 import NeumorphicLoginWrapper from '../styledComponents/NeumorphicLoginWrapper';
 
 
-const logoAnimation = keyframes`
+const loadingAnimation = keyframes`
     0%{background-position:0% 61%}
     50%{background-position:100% 40%}
     100%{background-position:0% 61%}
 `;
-
 
 const ImageWrapper = styled.div`
     width: 100px;
     height: 100px;
     border-radius: 50%;
     margin: 0 auto;
-    background: ${props => props.image ? `url(${props.image})` :  'linear-gradient(270deg, #d6d7d7, #a5a5a5, #d6d7d7, #a5a5a5)'};
-    -webkit-animation: ${logoAnimation} 1s ease infinite;
-    -moz-animation: ${logoAnimation} 1s ease infinite;
-    animation: ${logoAnimation} 1s ease infinite;
-    background-size: ${props => props.image ? '100px' :  '50000% 50000%'};
+    background: ${props => props.imageLoaded ? `url(${props.imageLoaded})` :  'linear-gradient(270deg, #e1e1e1, #cdcdcd, #e1e1e1, #cdcdcd, #e1e1e1, #cdcdcd)'};
+    -webkit-animation: ${loadingAnimation} 1s ease infinite;
+    -moz-animation: ${loadingAnimation} 1s ease infinite;
+    animation: ${loadingAnimation} 1s ease infinite;
+    background-size: ${props => props.imageLoaded ? '100px' :  '50000% 50000%'};
     display: flex;
     justify-content: center;
     align-items: center;
     box-shadow: 0px 0px 2px #ECF0F3, 0px 0px 0px 5px #ECF0F3, 8px 8px 15px #A7AAAF, -8px -8px 15px #FFFFFF;
 `;
 
+const TextWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
 const TitleText = styled.div`
     text-align: center;
     font-size: 20px;
-    padding-top: 24px;
     letter-spacing: 0.5px;
+    background: ${props => props.imageLoaded ? 'transparent' :  'linear-gradient(270deg, #e1e1e1, #cdcdcd, #e1e1e1, #cdcdcd, #e1e1e1, #cdcdcd)'};
+    -webkit-animation: ${loadingAnimation} 1s ease infinite;
+    -moz-animation: ${loadingAnimation} 1s ease infinite;
+    animation: ${loadingAnimation} 1s ease infinite;
+    background-size: ${props => props.imageLoaded ? '100px' :  '50000% 50000%'};
+    color: ${props => props.imageLoaded ? 'black': 'transparent'};
+    margin-top: 30px;
+    width: ${props => props.imageLoaded ? 'auto' : '150px'};
+    margin-bottom 20px;
+    border-radius: 25px;
 `;
+// box-shadow: ${props => props.imageLoaded ? '0px 0px 0px' : '13px 13px 20px #CBCED1, -13px -13px 20px #FFFFFF'};
 
 const SubTitleText = styled.div`
     text-align: center;
     font-size: 13px;
-    padding-top: 7px;
     font-weight: 600;
+    background: ${props => props.imageLoaded ? 'transparent' :  'linear-gradient(270deg, #e1e1e1, #cdcdcd, #e1e1e1, #cdcdcd, #e1e1e1, #cdcdcd)'};
+    -webkit-animation: ${loadingAnimation} 1s ease infinite;
+    -moz-animation: ${loadingAnimation} 1s ease infinite;
+    animation: ${loadingAnimation} 1s ease infinite;
+    background-size: ${props => props.imageLoaded ? '120px' :  '50000% 50000%'};
+    width: ${props => props.imageLoaded ? 'auto' : '90px'};
+    color: ${props => props.imageLoaded ?'black' : 'transparent'};
+    border-radius: 25px;
 `;
+// box-shadow: ${props => props.imageLoaded ? '0px 0px 0px' : '13px 13px 20px #CBCED1, -13px -13px 20px #FFFFFF'};
 
 class AuthenticatedDashboard extends Component {
     constructor(props){
@@ -60,16 +83,18 @@ class AuthenticatedDashboard extends Component {
         const { userData } = this.props;
         const userImage = userData.images ? userData.images[0].url : '';
         console.log('User data', userData);
-        const displayName = userData.display_name ? userData.display_name : '';
+        const displayName = userData.display_name ? userData.display_name : 'No name yet';
         const  email = userData.email ? userData.email : '';
-        const followerCount = userData.followers ? userData.followers.total : '';
+        const followerCount = userData.followers ? userData.followers.total : 'No';
         const userType = userData.type ? userData.type : '';
         const followerSentence = followerCount + ' followers';
         return (
             <NeumorphicLoginWrapper>
-                <ImageWrapper image={userImage} />
-                <TitleText> {displayName} </TitleText>
-                <SubTitleText> { followerCount !== '' && followerSentence} </SubTitleText>
+                <ImageWrapper imageLoaded={userImage} />
+                <TextWrapper>
+                    <TitleText imageLoaded={userImage} > {displayName} </TitleText>
+                    <SubTitleText imageLoaded={userImage}> { followerCount !== '' && followerSentence} </SubTitleText>
+                </TextWrapper>
             </NeumorphicLoginWrapper>
         )
     }
