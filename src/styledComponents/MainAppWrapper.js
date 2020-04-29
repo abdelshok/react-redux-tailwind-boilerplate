f31039f305accfe818637461425baaf21ee7de26
Abdel <abdelshokair@gmail.com>
2020-04-29 02:04:00 -0400

    Remove unnecessary border on app page

--- src/styledComponents/MainAppWrapper.js
+++ src/styledComponents/MainAppWrapper.js
@@ -6,9 +6,6 @@ const MainAppWrapper = styled.div`
     width: 100%;
     height: 100%;
     background-color: #ECF0F3;
-    border-style: solid;
-    border-width: 10px;
-    border-color: black;
     box-sizing: border-box;
     justify-content: center;
     display: flex;
